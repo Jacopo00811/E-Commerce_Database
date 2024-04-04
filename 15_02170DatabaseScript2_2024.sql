a62c8e571c87e342c65852e05c9611588a7c3e1c
MrMustii <mustii.elmadani@gmail.com>
2024-04-04 15:48:08 +0200

    Function, PROCEDURE and trigger

--- 15_02170DatabaseScript2_2024.sql
+++ 15_02170DatabaseScript2_2024.sql
@@ -1,10 +1,67 @@
 -- Give examples of a function, a procedure and a trigger and explain what they do. Give one example of each.
 
 -- FUNCTION
+-- Finds the total cost of a cart
+drop function if exists Total_in_Cart;
+
+DELIMITER //
+Create function Total_in_Cart(Cart_ID VARCHAR(320)) returns decimal(10,2)
+Begin
+Declare Result decimal(10,2);
+SELECT SUM(Price) into Result
+FROM Items
+JOIN Product ON Items.ProductID = Product.ProductID
+WHERE CartID = Cart_ID
+GROUP BY CartID;
+return Result;
+end//
+DELIMITER ;
+
+SELECT Total_in_Cart('Cart01');
+
+
 
 
 -- PROCEDURE
+-- Buy items in cart. means empty cart and decress the stock of the products 
+drop PROCEDURE if exists Buy;
+DELIMITER //
+CREATE PROCEDURE Buy (IN Cart_ID VARCHAR(320))
+BEGIN
+UPDATE Product 
+JOIN items ON Items.ProductID = Product.ProductID
+SET Stock = stock-1
+WHERE Product.ProductID in (SELECT Items.ProductID FROM Items WHERE CartID=Cart_Id);
+DELETE FROM Cart WHERE CartID = Cart_ID;
+end//
+DELIMITER ;
 
 
+call Buy ("Cart03");
+select * from items;
+select productid, stock from product;
+ 
 -- TRIGGER 
+-- If product is removed remove all its reviews and remove it from carts
+
+drop trigger if exists RemoveReviews;
+DELIMITER //
+create trigger RemoveReviews before delete on product
+For each row
+BEGIN
+DELETE FROM Review where Review.ProductID= OLD.Productid;
+DELETE FROM items where items.ProductID= OLD.Productid;
+end//
+DELIMITER ;
+
+
+
+delete from Product where ProductID="33";
+Select * from Review;
+select * from Items;
+
+
+
+
+
 
