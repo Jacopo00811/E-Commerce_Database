ffbd5a768e66e998f5827a07466d31ac99358501
Jacopo <jacopo.ceccuti@gmail.com>
2024-04-05 11:54:21 +0200

    Added UPDATE statement

--- 15_02170DatabaseScript2_2024.sql
+++ 15_02170DatabaseScript2_2024.sql
@@ -1,42 +1,20 @@
---Queries:
-SELECT CustomerEmail, CartID, COUNT(DISTINCT ProductID) AS Products,SUM(price) AS $Total 
-FROM creates NATURAL JOIN cart NATURAL JOIN items NATURAL JOIN product
-GROUP BY CartID
-
--- Get reviews ordered by review id
-SELECT ReviewID,ProductID,ProductName,Rating,Title,body,reviewDate 
-FROM Review NATURAL JOIN Product 
-WHERE ProductID IN (11)
-ORDER BY reviewDate
-
--- Get the average starrating of Products
-SELECT ProductName,AVG(Rating) 
-FROM Review NATURAL JOIN Product 
-GROUP BY ProductID
-HAVING AVG(Rating) > 2
-
-
-
-
-
-
 -- Give examples of a function, a procedure and a trigger and explain what they do. Give one example of each.
 
 -- FUNCTION
 -- Finds the total cost of a cart
-drop function if exists Total_in_Cart;
+DROP FUNCTION IF EXISTS Total_in_Cart;
 
 DELIMITER //
-Create function Total_in_Cart(Cart_ID VARCHAR(320)) returns decimal(10,2)
-Begin
-Declare Result decimal(10,2);
-SELECT SUM(Price) into Result
+CREATE FUNCTION Total_in_Cart(Cart_ID VARCHAR(320)) RETURNS DECIMAL(10,2)
+BEGIN
+DECLARE Result DECIMAL(10,2);
+SELECT SUM(Price) INTO Result
 FROM Items
 JOIN Product ON Items.ProductID = Product.ProductID
 WHERE CartID = Cart_ID
 GROUP BY CartID;
-return Result;
-end//
+RETURN Result;
+END//
 DELIMITER ;
 
 SELECT Total_in_Cart('Cart01');
@@ -46,42 +24,47 @@ SELECT Total_in_Cart('Cart01');
 
 -- PROCEDURE
 -- Buy items in cart. means empty cart and decress the stock of the products 
-drop PROCEDURE if exists Buy;
+DROP PROCEDURE IF EXISTS Buy;
 DELIMITER //
 CREATE PROCEDURE Buy (IN Cart_ID VARCHAR(320))
 BEGIN
 UPDATE Product 
 JOIN items ON Items.ProductID = Product.ProductID
 SET Stock = stock-1
-WHERE Product.ProductID in (SELECT Items.ProductID FROM Items WHERE CartID=Cart_Id);
+WHERE Product.ProductID IN (SELECT Items.ProductID FROM Items WHERE CartID=Cart_Id);
 DELETE FROM Cart WHERE CartID = Cart_ID;
-end//
+END//
 DELIMITER ;
 
 
-call Buy ("Cart03");
-select * from items;
-select productid, stock from product;
+CALL Buy ("Cart03");
+SELECT * FROM items;
+SELECT productid, stock FROM product;
  
 -- TRIGGER 
 -- If product is removed remove all its reviews and remove it from carts
 
-drop trigger if exists RemoveReviews;
+DROP TRIGGER IF EXISTS RemoveReviews;
 DELIMITER //
-create trigger RemoveReviews before delete on product
-For each row
+CREATE TRIGGER RemoveReviews BEFORE DELETE ON product
+FOR EACH ROW
 BEGIN
 DELETE FROM Review where Review.ProductID= OLD.Productid;
 DELETE FROM items where items.ProductID= OLD.Productid;
-end//
+END//
 DELIMITER ;
 
 
+-- DELETE STATEMENT
+DELETE FROM Product WHERE ProductID="33";
+SELECT * FROM Review;
+SELECT * FROM Items;
 
-delete from Product where ProductID="33";
-Select * from Review;
-select * from Items;
-
+-- UPDATE STATEMENT
+UPDATE Review
+SET Rating = 5, Title = "Best fish Ever", Body = "I love rotten fish" 
+WHERE ReviewID = "R4";
+SELECT * FROM Review;
 
 
 
